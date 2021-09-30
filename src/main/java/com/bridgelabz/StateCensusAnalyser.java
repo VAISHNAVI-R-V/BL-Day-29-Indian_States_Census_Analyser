97320949918ddf24618a4ae9206fbf4d8df1f239
VAISHNAVI-R-V <vaishucv14@gmail.com>
2021-09-30 09:12:06 +0530

    ADDED: Welcome Message

---
+++ src/main/java/com/bridgelabz/StateCensusAnalyser.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+/**
+ * @author: VAISHNAVI R. VISHWAKARMA.
+ * @purpose: To implement Indian States Census Analyse Program.
+ * @DATE: 9-September-2021.
+ */
+
+public class StateCensusAnalyser {
+    // Main method
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian State Census Analyser Program..!! ");
+    }
+}
